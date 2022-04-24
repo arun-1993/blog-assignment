0ee8993c683c92cee78fe2ae2271364b7c3274a1
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2022-04-24 16:44:01 +0530

    minor changes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            // return $this->redirectToRoute('target_path');
+            return $this->redirectToRoute('user_list');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
--- src/Service/Information.php
+++ src/Service/Information.php
@@ -9,12 +9,22 @@ use App\Repository\UserRepository;
 
 class Information
 {
-    public function overview(UserRepository $userRepository, CategoryRepository $categoryRepository, DishRepository $dishRepository, CommentRepository $commentRepository)
+    private $userRepository, $categoryRepository, $dishRepository, $commentRepository;
+
+    public function __construct(UserRepository $userRepository, CategoryRepository $categoryRepository, DishRepository $dishRepository, CommentRepository $commentRepository)
     {
-        $user_count = $userRepository->count([]);
-        $category_count = $categoryRepository->count([]);
-        $dish_count = $dishRepository->count([]);
-        $comment_count = $commentRepository->count([]);
+        $this->userRepository = $userRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->dishRepository = $dishRepository;
+        $this->commentRepository = $commentRepository;
+    }
+
+    public function overview()
+    {
+        $user_count = $this->userRepository->count([]);
+        $category_count = $this->categoryRepository->count([]);
+        $dish_count = $this->dishRepository->count([]);
+        $comment_count = $this->commentRepository->count([]);
 
         return array($user_count, $category_count, $dish_count, $comment_count);
     }
